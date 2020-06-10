c397bd69c70c6993fa8fa1a627410862100c0de4
patrickjwolf <patrickwolf@Lenovo-PC.localdomain>
2020-06-10 08:59:35 -0600

    Update

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,22 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Base Class
+class Vehicle(object):
+    pass
+class GroundVehicle(Vehicle):
+    pass
+class Car(GroundVehicle):
+    pass
+class Motorcycle(GroundVehicle):
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+class Starship(FlightVehicle):
+    pass
+class Airplane(FlightVehicle):
+    pass
